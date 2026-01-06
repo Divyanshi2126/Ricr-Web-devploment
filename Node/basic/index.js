36924c7b87d9d046847158d911238ed0350dd383
divyanshi2126 <divyanshichaurasiya68@gmail.com>
2026-01-06 19:48:57 +0530

    basic nodejs

---
+++ Node/basic/index.js
@@ -0,0 +1,17 @@
+// console.log("hello world");
+
+// let a =10;
+
+// if(a%2==0)
+// {
+//     console.log("even");
+// }else{
+//     console.log ("odd");
+// }
+
+function add(a,b){
+    console.log(a+b)
+}
+console.log("abcd");
+
+export default add;
