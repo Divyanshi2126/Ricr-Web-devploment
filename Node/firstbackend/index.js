e4e42f173d5b9781d3af8f639ebc6e116f5c809c
divyanshi2126 <divyanshichaurasiya68@gmail.com>
2026-01-08 19:11:09 +0530

    nodejs

---
+++ Node/firstbackend/index.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
+import express from 'express'
+
+const app = express();
+
+app.get("/",(req,res)=>
+{
+    console.log("server is running");
+    res.json({message: "servere is running sucessefully"})
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=>
+{
+    console.log ("server started at port ", port);
+});
