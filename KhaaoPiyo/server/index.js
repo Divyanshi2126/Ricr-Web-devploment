04924e105c37069ad6b87f6132a51a9398924a0f
divyanshi2126 <divyanshichaurasiya68@gmail.com>
2026-01-28 22:52:04 +0530

    KHAAOPIYO

--- KhaaoPiyo/server/index.js
+++ KhaaoPiyo/server/index.js
@@ -1,9 +1,9 @@
-import dotenv from "dotenv";
-dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import cloudinary from "./src/config/cloudinary.js";
 import connectDB from "./src/config/db.js";
 import AuthRouter from "./src/routers/authRouter.js";
 import UserRouter from "./src/routers/userRouter.js";
@@ -31,7 +31,18 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log("Server Started at Port: ", port);
   connectDB();
+
+  try {
+
+    const res = await cloudinary.api.ping();  
+    console.log("cloudinary API is working:" ,res);
+
+    
+  } catch (error) {
+    console.error("Error connecting api:",error);
+    
+  }
 });
