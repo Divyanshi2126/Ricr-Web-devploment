49ee4f9d719e8e3976b6183e17ec71bdc6d1d779
divyanshi2126 <divyanshichaurasiya68@gmail.com>
2026-01-29 20:04:31 +0530

    KHAAOPIYO

--- KhaaoPiyo/server/src/routers/userRouter.js
+++ KhaaoPiyo/server/src/routers/userRouter.js
@@ -3,9 +3,11 @@ import express from "express";
 import {Userupdate} from "../controller/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import multer from "multer";
+import e from "express";
 
 const router = express.Router();
 
 router.put("/update",protect,Userupdate);
 
 export default router;
+
